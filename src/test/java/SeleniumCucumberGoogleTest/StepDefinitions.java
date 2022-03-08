022bedeb3f9176bb372a6786ddb839217b93399e
Yasin Sönmez <{ID}+{username}@users.noreply.github.com>
2022-03-08 08:35:32 +0100

    first commit

---
+++ src/test/java/SeleniumCucumberGoogleTest/StepDefinitions.java
@@ -0,0 +1,72 @@
+package SeleniumCucumberGoogleTest;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AreThereMoreThanEightResults {
+    static String areThereMoreThanEightResults(int resultCount) {
+        return resultCount > 8 ? "yes" : "no, there are less";
+    }
+}
+
+public class StepDefinitions {
+
+    private WebDriver driver;
+    private String actualAnswer;
+    private String expectedAnswer;
+
+    @Given("I search for Selenium in Google")
+    public void goToGoogleAndSearchForSelenium() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.de/");
+        String title = driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        // Verify
+        Assertions.assertEquals("Google", driver.getTitle());
+
+        // Depending on browser configuration and/or location you could be instructed to
+        // confirm usage of cookies
+        WebElement cookieConfirmation = driver.findElement(By.id("L2AGLb"));
+        if (cookieConfirmation.isDisplayed()) {
+            cookieConfirmation.click();
+        }
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        searchBox.sendKeys("Selenium");
+        searchButton.submit();
+    }
+
+    @When("I ask whether there are more than 8 results")
+    public void askWhetherThereAreMoreThanEightResults() {
+        List<WebElement> foundElementsOnFirstPage = driver.findElements(By.className("g"));
+        int numberOfResults = foundElementsOnFirstPage.size();
+        actualAnswer = AreThereMoreThanEightResults.areThereMoreThanEightResults(numberOfResults);
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Then("I should be told {string}")
+    public void iShouldBeTold(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
---
+++ src/test/resources/SeleniumCucumberGoogleTest/how_many_results_are_there.feature
@@ -0,0 +1,7 @@
+Feature: Are there more than 8 search results for Selenium?
+  Everybody wants to know if there are more than 8 results.
+
+ Scenario: Typing Selenium in Google gives more than 8 results
+   Given I search for Selenium in Google
+   When I ask whether there are more than 8 results
+   Then I should be told "yes"
